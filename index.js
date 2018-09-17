041544ba4c437bb13518f5f14c27f2e48c5e931d
laudeon <louis.audeon@mail.be>
2018-09-17 15:55:45 +0200

    bot refactored

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+// @see the botkit documentation for more infos regarding the controller and bot API
+
 'use strict'
 
 require('dotenv').config({ silent: process.env.NODE_ENV === 'production' })
 const io = require('socket.io')(process.env.PORT || 4000)
 
 const controller = require('./lib/controller')
-const webSocketEvents = require('./lib/webSocketEvents')
+const websocket = require('./lib/websocket')
 const chatbot = require('./lib/chatbot')
 const recastaiMiddleware = require('./lib/middlewares/recastai')
 
-controller.startTicking()
-io.on('connection', webSocketEvents.vroum(controller))
-chatbot.vroum(controller, recastaiMiddleware)
+controller.startTicking() // Start controller
+io.on('connection', websocket.vroum(controller)) // start listening for incominn message event and delegate message to controller
+chatbot.vroum(controller, recastaiMiddleware) // Init chatbot logic
